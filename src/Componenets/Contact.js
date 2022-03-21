c51fb87515d93eac1b5e0b29173a97f0657d8600
moefarts <moefarts@my.yorku.ca>
2022-03-20 22:26:27 -0400

    styling

--- src/Componenets/Contact.js
+++ src/Componenets/Contact.js
@@ -17,17 +17,17 @@ function Contact() {
                 <form action={formAction} method="POST"  onClick={formOnClick} className="contactForm">
                     <div className="inputContainer">
                         <label htmlFor="nameInput" className="nameInput input"> Name </label>
-                        <input name="Name" required type="text" id = "nameInput" placeholder="John Smith"></input>
+                        <input className="input nameInput" name="Name" required type="text" id = "nameInput" placeholder="John Smith"></input>
                     </div>
 
                     <div className="inputContainer">
                         <label htmlFor="emailInput" className="nameInput input"> Email </label>
-                        <input name="Email" required type="email" id = "emailInput" placeholder="example@example.com"></input>
+                        <input className="input emailInput" name="Email" required type="email" id = "emailInput" placeholder="example@example.com"></input>
                     </div>
 
                     <div className="inputContainer messageInputContainer">
                         <label htmlFor="messageInput" className="messageInputLabel input"> Message </label>
-                        <textarea name="Message" required className="messageInput" type="textarea" id = "messageInput" placeholder="Message"></textarea>
+                        <textarea  name="Message" required className=" input messageInput" type="textarea" id = "messageInput" placeholder="Message"></textarea>
                     </div>
 
                     <button className="submitButton" type="submit"> Submit</button>
