5948db5579e926e880728fb53119413411c9b699
moefarts <moefarts@my.yorku.ca>
2022-03-27 21:03:49 -0400

    dep

--- src/Componenets/AboutMe.js
+++ src/Componenets/AboutMe.js
@@ -7,7 +7,10 @@ function AboutMe() {
     <div id = "aboutMe" className="aboutMeContainer">
         <div className="aboutText">
             <h1 className="aboutMeHeader">About Me </h1>
-            <p className="aboutMeText"> Hi! I'm Mohanad but you can call me Mo. I am Front-End Developer based out of Toronto.
+            <p className="aboutMeText"> Hi! I'm Mohanad but you can call me Mo. 
+                                        <br></br>
+                                        <br></br>
+                                        I am Front-End Developer based out of Toronto.
                                         I bring the discipline, creativity and analytical rigour of the science of chemistry 
                                         to Front-End Development.
                                         <br></br>
