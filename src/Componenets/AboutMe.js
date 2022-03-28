78cced898159b008856edd567aa754c2441f8323
moefarts <moefarts@my.yorku.ca>
2022-03-27 20:46:47 -0400

    final

--- src/Componenets/AboutMe.js
+++ src/Componenets/AboutMe.js
@@ -7,18 +7,15 @@ function AboutMe() {
     <div id = "aboutMe" className="aboutMeContainer">
         <div className="aboutText">
             <h1 className="aboutMeHeader">About Me </h1>
-            <p className="aboutMeText"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                        Aenean quis urna tincidunt sem cursus interdum a eu eros. 
-                                        Nulla varius sed erat faucibus hendrerit.
-                                        Nullam consequat nulla ante, nec commodo nibh feugiat non. 
-                                        Aenean non ullamcorper eros. Donec elit nulla, 
-                                        vulputate lacinia lectus ac, ornare auctor neque. Curabitur elementum 
-                                        risus nunc, vitae tempor mauris maximus a. 
-                                        Vivamus ut lobortis nisi, ut viverra arcu. Integer erat nibh, tempor et 
-                                        finibus nec, elementum congue lacus. 
-                                        Nulla facilisi. Orci varius natoque penatibus et magnis
-                                        dis parturient montes, nascetur ridiculus mus. Sed mole
+            <p className="aboutMeText"> Hi! I'm Mohanad but you can call me Mo. I am Front-End Developer based out of Toronto.
+                                        I bring the discipline, creativity and analytical rigour of the science of chemistry 
+                                        to Front-End Development.
+                                        <br></br>
+                                        <br></br>
+                                        Besides code and chemistry I enjoy: Brazilian Jiu Jitsu,
+                                        Fitness, developing new skills and cheesecake
             </p>
+
         </div>
         <div className="aboutImage"><img className="flowerPic" src={flower} alt ="flower"/></div>
     
